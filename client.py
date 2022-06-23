95ec0c0b2a53a089d46bc4d94fb99c30f10738ea
Евгения <58876871+Sunwait-nsk@users.noreply.github.com>
2022-06-23 20:28:06 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,29 @@
+import socket
+import secrets
+import string
+
+
+sock = socket.socket()
+sock.connect(('localhost', 9090))
+server_address = ('localhost', 9090)
+print('Подключено к {} порт {}'.format(*server_address))
+sock.send('hello, world!'.encode())
+
+try:
+    for _ in range(10000):
+        bbbb = f'{secrets.randbelow(9999):04}' # BBBB - номер участника
+        nn = f"C{secrets.choice(string.digits)}"  # NN - id канала
+        hh = f'{secrets.randbelow(6):02}'  # HH - Часы
+        mm = f'{secrets.randbelow(60):02}'  # MM - минуты
+        ss = f'{secrets.randbelow(60):02}'  # SS - секунды
+        zhq = f'{secrets.randbelow(1000):03}'  # zhq - десятые сотые тысячные
+        gg = f'{secrets.randbelow(20):02}'  # GG - номер группы
+        message = f'{bbbb} {nn} {hh}:{mm}:{ss}.{zhq} {gg}\n'
+        # if len(message.encode()) < 100:
+        #     sock.sendall(f'{(len(message.encode())):02}'.encode())
+        #     sock.sendall(message.encode())
+        sock.sendall(message.encode())
+
+finally:
+    print('Закрываем сокет')
+    sock.close()
---
+++ server.py
@@ -0,0 +1,40 @@
+import logging
+import re
+import socket
+
+# создаемTCP/IP сокет
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Привязываем сокет к порту
+server_address = ('localhost', 9090)
+print('Старт сервера на {} порт {}'.format(*server_address))
+sock.bind(server_address)
+
+# Слушаем входящие подключения
+sock.listen(1)
+
+tail = ''
+
+logging.basicConfig(filename="sample.log", level=logging.INFO)
+logger = logging.getLogger("server")
+logger.setLevel(logging.INFO)
+while True:
+    print('Ожидание соединения...')
+    connection, client_address = sock.accept()
+    try:
+        print('Подключено к:', client_address)
+        while data := connection.recv(4096).decode("utf-8"):
+            records = f'{tail}{data}'.split('\n')
+
+            for record in records:
+                if len(record) == 23:
+                    bbbb, nn, hh, mm, ss_zsh, gg = re.split(' |:', record)
+                    if gg == '00':
+                        print(f'cпортсмен, нагрудный номер {bbbb} прошёл отсечку {nn} в «{hh}:{mm}:{ss_zsh[:4]}»')
+                    logging.info(record)
+                else:
+                    tail = record
+
+    except ValueError:
+        connection.close()
+        logging.info('ValueError. Connection close.')
